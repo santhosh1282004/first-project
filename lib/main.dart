a255fa2ec28eddd19c4efe041a1cbcd4cb889831
santhosh1282004 <san1282004@gmail.com>
2026-01-09 09:34:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title:Text("hellow wold",
+        style:TextStyle(color:Colors.green),),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+    )
+  ));
+}
+
